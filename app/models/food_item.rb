33ea0e13865a0323905201cd788bd72ea8b6c5b7
Minh Dinh <dtminhpt@gmail.com>
2016-03-14 10:04:13 +0700

    Add missing model

---
+++ app/models/food_item.rb
@@ -0,0 +1,14 @@
+class FoodItem < ActiveRecord::Base
+	def image_url_or_auto
+		if image_url.present?
+			image_url
+		else
+			"http://loremflickr.com/320/240/#{name}"
+		end
+		# image_url.presence || "http://loremflickr.com/320/240/#{name}"
+	end 
+  
+  def self.filter_by_section(section)
+    where("section ILIKE ?", "%#{section}%")
+  end
+end
