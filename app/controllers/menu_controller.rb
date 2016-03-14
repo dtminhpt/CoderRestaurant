af8f91b9fd785edef094ef3c86dc25704f840930
Minh Dinh <dtminhpt@gmail.com>
2016-03-14 15:13:33 +0700

    edit Readme.md

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,6 +1,6 @@
 class MenuController < ApplicationController
   def index
-		@sections = %w(Breakfast Lunch Dinner Supper)
+		@sections = %w(Breakfast Lunch Dinner Drinks)
 		@food_items = FoodItem.filter_by_section(params[:section])
     if params[:sort].present?
       @food_items = @food_items.order(params[:sort])
