ecd6f4e085614cad85bd9135c43e24063d18c377
Minh Dinh <dtminhpt@gmail.com>
2016-03-14 09:34:30 +0700

    Add option for sorting by name and price

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,6 +1,9 @@
 class MenuController < ApplicationController
   def index
-  		@sections = %w(Breakfast Lunch Dinner Supper)
-  		@food_items = FoodItem.filter_by_section(params[:section])
+		@sections = %w(Breakfast Lunch Dinner Supper)
+		@food_items = FoodItem.filter_by_section(params[:section])
+    if params[:sort].present?
+      @food_items = @food_items.order(params[:sort])
+    end
   end
 end
